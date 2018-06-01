235768115d62ad6b763b3bb136ea4378a7e2f166
Guest User <Guest@Epicodus-7E.local>
2018-06-01 15:05:23 -0700

    add background color. variables for chosen picture. A button for each quiz question.

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,26 @@ $(document).ready(function() {
   $("#pictureAnswersC").click(function() {
     var pictureAnswer = 10
     $(".pictureChoice").hide();
-  })
+  });
   $("#pictureAnswersRUBY").click(function() {
     var pictureAnswer = 20
     $(".pictureChoice").hide();
-  })
+  });
+
   $("#pictureAnswersREACT").click(function() {
     var pictureAnswer = 30
     $(".pictureChoice").hide();
-  })
-});
-$("form#written-form").submit(function(event) {
-  var writtenAnswer = $("input#written-imput1").val()
+  });
 
-  event.preventDeafault();
-});
+  $("form#written-form").submit(function() {
+  var writtenAnswer = $("input#written-imput").val()
+  });
 
-var chosenFriend = $("input:radio[name=friendChoice]:checked").val()
+  $("#questionButton3").click(function() {
+    $(".radio").hide();
+  });
+
+  var chosenFriend = $("input:radio[name=friendChoice]:checked").val()
+
+  event.preventDefault();
+});
