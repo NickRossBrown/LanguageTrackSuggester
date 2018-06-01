83f11e0da6c7897e32ac4fe2f3d20dede5cf3912
Guest User <Guest@Epicodus-7E.local>
2018-06-01 13:50:16 -0700

    add format quiz questions in html. Page layout issues resolved

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+$("form#written-form").submit(function(event) {
+  var writtenAnswer = $("input#written-imput1").val()
+
+  event.preventDeafault();
+});
+
+var chosenFriend = $(input:radio[name=friendChoice]:checked).val())
