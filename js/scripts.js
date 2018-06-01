efe6c50d9430a10945f96e6d97d7e164ba6e68d0
Guest User <Guest@Epicodus-7E.local>
2018-06-01 14:30:28 -0700

    add a picture resumbling each program. Clicking a picture is part of the language quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
-
+$(document).ready(function() {
+  $("#pictureAnswersC").click(function() {
+    var pictureAnswer = 10
+    $(".pictureChoice").hide();
+  })
+  $("#pictureAnswersRUBY").click(function() {
+    var pictureAnswer = 20
+    $(".pictureChoice").hide();
+  })
+  $("#pictureAnswersREACT").click(function() {
+    var pictureAnswer = 30
+    $(".pictureChoice").hide();
+  })
+});
 $("form#written-form").submit(function(event) {
   var writtenAnswer = $("input#written-imput1").val()
 
   event.preventDeafault();
 });
 
-var chosenFriend = $(input:radio[name=friendChoice]:checked).val())
+var chosenFriend = $("input:radio[name=friendChoice]:checked").val()
