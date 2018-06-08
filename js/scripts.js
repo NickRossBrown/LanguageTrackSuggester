8ac59106a6c1a19b7ed04ce2535f27f34656cc4f
Guest User <Guest@Epicodus-7A.local>
2018-06-07 22:14:37 -0700

    add another question

--- js/scripts.js
+++ js/scripts.js
@@ -21,13 +21,8 @@ $(document).ready(function() {
       reactPoints+=1
       answerpoints= answerpoints / 7
       console.log("react ponts  " + reactPoints)
-  }
-}
-  // function pointWinner(winner){
-  //   if (rubyPoints > reactPoints && rubyPoints > cSharpPoints){
-  //   }
-  // }
-
+      }
+    }
   $("#pictureAnswersC").click(function() {
     countPoints(5)
     $("#pictureAnswersC").hide();
@@ -71,48 +66,43 @@ $(document).ready(function() {
     $("#written-form").hide()
     event.preventDefault();
   });
-
   $("#questionButton3").click(function() {
     chosenFriend = parseInt($("input:radio[name=friendChoice]:checked").val())
     console.log(chosenFriend)
     countPoints(chosenFriend)
     $(".radio").hide();
   });
+  $("#questionButtonTwo").click(function() {
+    var selectedAnswer = parseInt($("#questionenTwo").val())
+    answerpoints*=selectedAnswer
+    countPoints(selectedAnswer)
+    $("#questionButtonTwo").hide()
+    $("#questionTwo").hide()
+  event.preventDefault();
+  });
+
   $("#finishedButton").click(function() {
-      if ((rubyPoints > reactPoints) && (rubyPoints > cSharpPoints)){
+      if ((rubyPoints >= reactPoints) && (rubyPoints >= cSharpPoints)){
         $(".answerRuby").show()
+        $("#finishedButton").hide()
       }
       if ((reactPoints > rubyPoints) && (reactPoints > cSharpPoints)){
-        $(".answerREACT").show()
+        $("#finishedButton").show()
       }
       if ((cSharpPoints > reactPoints) && (cSharpPoints > rubyPoints)){
-        $(".answerC").show()
+        $("#finishedButton").show()
       }
     })
-    // console.log(answerpoints)
-    // rubyPoints=0//3
-    // reactPoints=0 //7
-    // cSharpPoints=0 //5
-    // while (answerpoints%3===0){
-    //   rubyPoints+=1
-    //   answerpoints= answerpoints / 3
-    //   console.log("ruby ponts  " + rubyPoints)
-    //   console.log("anserpoints" + answerpoints)
-    // }
-    // while (answerpoints%5===0){
-    //   cSharpPoints+=1
-    //   answerpoints= answerpoints / 5
-    //   console.log("c shartp ponts  " + cSharpPoints)
-    //   console.log("anserpoints" + answerpoints)
-    // }
-    // while (answerpoints%7===0){
-    //   reactPoints+=1
-    //   answerpoints= answerpoints / 7
-    //   console.log("react ponts  " + reactPoints)
-    // }
-    // if (writtenAnswer > 1){
-    //   $(".answerC").show()
-    // }
+  $(".restartWebpage1").click(function() {
+    console.log("tetset")
+
+    $(".answerRuby").hide()
+    $(".answerREACT").hide()
+    $(".answerC").hide()
+    $("#finishedButton").show()
+
+    })
+
 
 
   // chosenFriend = $("input:radio[name=friendChoice]:checked").val()
